331425326f6cb62fdd4377e2ff4d73932065d7dd
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-04 14:24:35 +0300

    Task2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+void Div(int num1, int num2)
+{
+    if (num1 % num2 ==0)
+        Console.WriteLine("кратно");
+    else
+        Console.WriteLine("не кратно" + num1 % num2 );
+}
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+Div(a, b);
