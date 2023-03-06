1b05bbd8b593f9440f7da625e5bf4c27e9d7e534
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-06 23:41:37 +0300

    HW3_2

--- Lesson_3/HW_3/HW3_2/Program.cs
+++ Lesson_3/HW_3/HW3_2/Program.cs
@@ -1,4 +1,5 @@
-﻿double dis(double x1, double y1, double z1, double x2, double y2,double z2)
+﻿
+double dis(double x1, double y1,double z1, double x2, double y2,double z2)
 {
     return Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
 }
