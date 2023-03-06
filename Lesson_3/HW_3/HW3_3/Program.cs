a41a51720630b300a9dac3a4d484d0101173594d
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-06 23:28:12 +0300

    HW3_3

---
+++ Lesson_3/HW_3/HW3_3/Program.cs
@@ -0,0 +1,13 @@
+﻿int num = int.Parse(Console.ReadLine()!);
+
+void Div(int n)
+{
+    int i = 1;
+    while(n >= i)    
+    {
+        Console.WriteLine(Math.Pow(i,3));
+        i ++;
+    }
+}
+
+Div(num);
