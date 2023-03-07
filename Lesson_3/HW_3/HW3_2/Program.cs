86ca8b99e7fb7e578b665d1b1a1fca8638d5ce1b
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-07 22:57:15 +0300

    HW3_3

--- Lesson_3/HW_3/HW3_2/Program.cs
+++ Lesson_3/HW_3/HW3_2/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 double dis(double x1, double y1,double z1, double x2, double y2,double z2)
+
 {
     return Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)));
 }
--- Lesson_3/HW_3/HW_3_1/Program.cs
+++ Lesson_3/HW_3/HW_3_1/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int num = int.Parse(Console.Read("")!);
+//int numL1 =
+//int numL2 =
+//int numR1= 
+//int numR2=
+
+//void Div(int n)
+//{
+  //  int i = 1;
+   // while(n >= i)    
+    //{
+    //    Console.WriteLine(Math.Pow(i,3));
+    //    i ++;
+    //}
+//}
+
+//Div(num);
