081ab5f8ca6cd5540e0bccfc5dbb1ecfb9ee8988
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-08 18:39:20 +0300

    HW3_1

--- Lesson_3/HW_3/HW_3_1/Program.cs
+++ Lesson_3/HW_3/HW_3_1/Program.cs
@@ -1,19 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int num = int.Parse(Console.Read("")!);
-//int numL1 =
-//int numL2 =
-//int numR1= 
-//int numR2=
-
-//void Div(int n)
-//{
-  //  int i = 1;
-   // while(n >= i)    
-    //{
-    //    Console.WriteLine(Math.Pow(i,3));
-    //    i ++;
-    //}
-//}
-
-//Div(num);
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет,
+// является ли оно палиндромом.14212 -> нет 12821 -> да 23432 -> да
+Console.WriteLine("Введите пятизначное число:");
+int num = int.Parse(Console.ReadLine()!);
+int num5 = num %10;
+int num4 = num / 10 % 10;
+int num2 = num /1000 %10;
+int num1 = num /10000;
+if (num1 == num5 && num2 ==num4)
+{
+    Console.WriteLine("число является палиндромом");
+}
+else
+{
+  Console.WriteLine("число не является палиндромом");  
+}
