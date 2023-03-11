d04df49543a3f30a1cbefe2a410cfb6f21d6b974
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-11 18:49:07 +0300

    HW4_1

--- Lesson_4/HW4/HW4_1/Program.cs
+++ Lesson_4/HW4/HW4_1/Program.cs
@@ -1,4 +1,30 @@
 ﻿// Напишите цикл, который пинимает на вход два числа(А и В) и
 //возводит чисо А в натуральную степень В
-Console.WriteLine('Введите Ваше число');
+Console.WriteLine("Введите Ваше число А ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите Ваше число В ");
+int b = int.Parse(Console.ReadLine()!);
+
+int DegNum(int c)
+{
+    int degrynum = a;
+    for(int deg = 1; deg < b; deg++)
+
+     degrynum *= a;
+
+    return degrynum;
+} 
+
+
+int result = DegNum(a);
+Console.Write(a);
+Console.Write(" в степени ");
+Console.Write(b);
+Console.Write(" равно ");
+Console.Write(result);
+
+
+
+
+
 
