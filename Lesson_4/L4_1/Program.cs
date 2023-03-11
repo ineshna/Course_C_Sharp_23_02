52c7f77092c886fcfbdfb462bbde4e51680a6ddf
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-11 13:51:50 +0300

    L4_1

---
+++ Lesson_4/L4_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// напишите программа,которая принимает на вход число и 
+//выдает количество цифр в числе.
+
+Console.WriteLine("Введите число");
+// напишите программу, которая приримает
+//на вход число(А) и выдает сумму числе от 1 до А
+
+int LengthNum(int num)
+{
+    int a = 0;
+    for(int i = 1;num > 0; i++)
+       {num = num/10;
+       a++;
+       }
+    return a;
+}    
+int num = int.Parse(Console.ReadLine()!);
+int result = LengthNum(num);
+Console.Write("число ");
+Console.Write(num);
+Console.Write(" состоит из ");
+Console.Write(result);
+Console.Write(" цифр");
