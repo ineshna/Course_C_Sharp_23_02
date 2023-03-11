8a28d7aaac0cc79e485ebf33c5154d537d93e8ec
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-11 14:33:14 +0300

    L4_2

---
+++ Lesson_4/L4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программму, которая принмиает на вход число N и
+//выдает произведение от 1 до N
+Console.WriteLine("Введите число");
+int CompositionNum(int num)
+{
+    int m= 1;
+    for(int i = 1; i <= num; i++)
+
+     m *= i;
+
+    return m;
+} 
+
+int a = int.Parse(Console.ReadLine()!);
+int result = CompositionNum(a);
+Console.Write("произведение числа ");
+Console.Write(a);
+Console.Write(" является ");
+Console.Write(result);
