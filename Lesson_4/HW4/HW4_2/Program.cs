ddea83c56963b87c2d3212df01ed7fbb3914cddf
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-11 20:29:39 +0300

    HW4_2

---
+++ Lesson_4/HW4/HW4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу,которая пинимает на вход число и 
+//выдает сумму цифр в числе
+Console.WriteLine("Введите Ваше число  ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNum(number));
+int SumNum(int num)
+{
+    int allsum = 0;
+    while (num > 0)
+    {
+        allsum += num % 10;
+        num /=10;
+
+    }
+    return allsum;
+}
