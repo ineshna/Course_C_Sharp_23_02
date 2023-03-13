012c8e01f2469affe84d9f76f939566f909227b4
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-13 12:00:16 +0300

    L6_3

---
+++ Lesson_6/L6_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// 3. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//    Первые два числа Фибоначчи: 0 и 1
+
+void Fib(int a)
+{
+    int firstNum = 0;
+    int secNum = 1;
+    for (int i = 0; i < a; i++)
+    {
+        Console.Write(firstNum + " ");
+        (firstNum, secNum) = (secNum, firstNum + secNum);
+    }
+}
+int a = int.Parse(Console.ReadLine()!);
+Fib(a);
+
+
+
+// 3. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+//    Первые два числа Фибоначчи: 0 и 1.
+
+
+//void Fib(int a)
+//{
+//    int festNum = 0;
+//    int secontNum = 1;
+//    for (int i = 0; i < a; i++)
+//    {
+//        Console.Write(festNum + " ");
+//        (festNum, secontNum) = (secontNum, festNum + secontNum);
+///    }
+//}
+//int A = int.Parse(Console.ReadLine()!);
+//Fib(A);
