ae1d56444e071956ac0935a47d8bd34e134639c2
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-16 23:29:13 +0300

    L6_4

--- Lesson_6/L6_4/Program.cs
+++ Lesson_6/L6_4/Program.cs
@@ -16,6 +16,7 @@ int[,] FillMass(int rows, int columns, int start, int end)
 }
 
 void PrinOff(int[,]arr){
+    
     for (int i = 0; i < arr.GetLength(0); i++){
         for (int j = 0; j < arr.GetLength(1); j++)
         {
