e857477b164e4a9824d0bdcfcadfbaab174dc0e1
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-16 23:26:17 +0300

    HW6_1

--- Lesson_6/HW6/HW6_1/Program.cs
+++ Lesson_6/HW6/HW6_1/Program.cs
@@ -46,3 +46,20 @@ FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
+
+int Check(int num)
+{
+int count = 0;
+
+    for( int i =1; i <= num; i++)
+    {
+        Console.Write("$ Число{i}: ");
+        int a =int.Parse(Console.ReadLine()!);
+        if (a > 0)count++;
+    }
+    return count;
+}
+Console.Write("Сколько чисел введете? :");
+int a = int.Parse(Console.ReadLine()!);
+int  result = Check(a);
+Console.WriteLine("$Количество положительных числ :{result}")
