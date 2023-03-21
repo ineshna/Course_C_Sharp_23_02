dba2843d73be70553b5f32d91ddb844607e08b2d
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-21 21:41:20 +0300

    L7_1

---
+++ Lesson_7/L7_1/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте двумерный массив размера m на n, каждый элеент массива
+//находится по формуле:А =m + n
+//Выведите полученный массив на экран.
+	void Print(int[,] arr)
+	{
+	    int row = arr.GetLength(0);
+	    int column = arr.GetLength(1);
+	
+	    for (int i = 0; i < row; i++)
+	    {
+	        for (int j = 0; j < column; j++)
+	            Console.Write($" {arr[i, j]} ");
+	        Console.WriteLine();
+	    }
+	}
+	
+	int[,] MassNums(int row, int column, int from, int to)
+	{
+	    int[,] arr = new int[row, column];
+	
+	    for (int i = 0; i < row; i++)
+	        for (int j = 0; j < column; j++)
+	            arr[i, j] = i + j;
+	    return arr;
+	}
+	
+	int row_num = int.Parse(Console.ReadLine()!);
+	int column_num = int.Parse(Console.ReadLine()!);
+	int start = int.Parse(Console.ReadLine()!);
+	int stop = int.Parse(Console.ReadLine()!);
+	
+	int[,] mass = MassNums(row_num, column_num, start, stop);
+	Print(mass);
+
