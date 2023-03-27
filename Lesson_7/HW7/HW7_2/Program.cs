b318f1e4c842840c86f7a22bb992559ed8731e71
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-27 17:47:58 +0300

    L8_2

--- Lesson_7/HW7/HW7_2/Program.cs
+++ Lesson_7/HW7/HW7_2/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Напишите программу, которая на вход принимает позиции 
 //элемента в двумерном массиве, и возвращает значение этого
 // элемента или же указание, что такого элемента нет.
-
 Console.WriteLine("Hello, World!");
 	void Print(int[,] arr)
 	{
@@ -16,7 +15,7 @@ Console.WriteLine("Hello, World!");
 	    }
         Console.WriteLine();
 	}
-	
+	//запалнящий массив
 	int[,] MassNums(int row, int column, int from, int to)
 	{
 	    int[,] arr = new int[row, column];
@@ -27,19 +26,17 @@ Console.WriteLine("Hello, World!");
 	    return arr;
 	}
 
-    string SerchEl(int[,]arr, int numi, int numj)
+    string SerchNum (int[,]arr, int num)
     {
         int row = arr.GetLength(0);
 	    int column = arr.GetLength(1);
-		if (numi > arr[row,column])
-		  Console.WriteLine("Такого числа нет");
 
     
         for (int i = 0; i < row; i++)
 	        for (int j = 0; j < column; j++)
 
-             if( arr[i-1,j-1] == numi && arr[i-1,j-1] == numj)
-              return $"на позиции [{numi}{numj}] стоит [{arr[i-1,j-1]}]";// если бы тут был consol,...то проверил все вхождения
+             if( arr[i,j] == num)
+              return $"[{i+1},{j+1}]";// если бы тут был consol,...то проверил все вхождения
     return "Такого числа нет";   
     }
 
@@ -52,8 +49,7 @@ Console.WriteLine("Hello, World!");
 
 	int[,] mass = MassNums(row_num, column_num, start, stop);
     Print(mass);
-    int vali = int.Parse(Console.ReadLine()!);
-	int valj = int.Parse(Console.ReadLine()!);
+    int val = int.Parse(Console.ReadLine()!);
 
-    string result = SerchEl(mass, vali, valj);
+    string result = SerchNum(mass, val);
     Console.WriteLine(result);
