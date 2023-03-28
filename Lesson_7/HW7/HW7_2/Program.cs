89f366d014ba83e56f20f8dd325e8f18efbe22a7
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-03-28 09:21:10 +0300

    HW7_2

--- Lesson_7/HW7/HW7_2/Program.cs
+++ Lesson_7/HW7/HW7_2/Program.cs
@@ -32,25 +32,24 @@ Console.WriteLine("Hello, World!");
         int row = arr.GetLength(0);
 	    int column = arr.GetLength(1);
 		if (numi > row-1 && numj >column-1)
-
-    
-        for (int i = 0; i < row; i++)
-	        for (int j = 0; j < column; j++)
-
-              return $"на позиции [{numi},{numj}] стоит число {arr[numi-1,numj-1]}";// если бы тут был consol,...то проверил все вхождения
-    return "Такого числа нет";   
+		Console.WriteLine("такого числа нет");  
+	return $"на позиции [{numi},{numj}] стоит число {arr[numi-1,numj-1]}";
     }
 
-	
+	Console.WriteLine("Введите число строк массива :");
 	int row_num = int.Parse(Console.ReadLine()!);
+	Console.WriteLine("Введите число столбцов массива :");
 	int column_num = int.Parse(Console.ReadLine()!);
+	Console.WriteLine("Введите диапазон массива :");
 	int start = int.Parse(Console.ReadLine()!);
 	int stop = int.Parse(Console.ReadLine()!);
 	
 
 	int[,] mass = MassNums(row_num, column_num, start, stop);
     Print(mass);
+	Console.WriteLine("Введите первый индекс искомого числа :");
     int vali = int.Parse(Console.ReadLine()!);
+	Console.WriteLine("Введите второй индекс искомого числа :");
 	int valj = int.Parse(Console.ReadLine()!);
 
     string result = SerchEl (mass, vali, valj);
