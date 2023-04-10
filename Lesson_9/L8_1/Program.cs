08eefdd1059a9189fd8914d0735669b44547fbc5
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-04-10 23:39:54 +0300

    L9_1

---
+++ Lesson_9/L8_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задайте значение N. Напишите программу, которая 
+//выведет все натуральные числа в промежутке 
+//от 1 до N при помощи рекурсии.
+
+void SeriesNums(int N)
+{
+    
+    if(N == 0) return;
+    SeriesNums(N - 1);
+    Console.Write($"{N}, ");
+}
+SeriesNums(int.Parse(Console.ReadLine()!));
