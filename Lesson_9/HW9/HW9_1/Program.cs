4341ceea27fde78418fbcbcd3987814115d16331
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-04-12 01:55:17 +0300

    HW9_1

--- Lesson_9/HW9/HW9_1/Program.cs
+++ Lesson_9/HW9/HW9_1/Program.cs
@@ -1,3 +1,16 @@
 ﻿//  Задайте значения M и N. Напишите программу, которая выведет все чётные
 //натуральные числа в промежутке от M до N с помощью рекурсии.
 
+
+void SeriesEven(int M, int N)
+{
+    if( M % 2 != 0) M = M+1;     
+     if(M > N) return;
+    Console.Write($"{M}, ");
+    SeriesEven(M+2,N) ;
+}
+Console.Write("Введите число N :");
+int numN = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число M :");
+int numM = int.Parse(Console.ReadLine()!);
+SeriesEven(numN, numM);
