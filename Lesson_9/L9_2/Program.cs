e09d8fd98fff53889cfcc23418c28f576d5ca122
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-04-12 00:03:16 +0300

    L9_2

---
+++ Lesson_9/L9_2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N. Напишите программу, которая
+//рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+
+void NumsFromTo(int M, int N)
+{
+    
+    if(M > N) return;
+    NumsFromTo(M,N - 1);
+    Console.Write($"{N}, ");
+}
+Console.Write("Введите число M :");
+Console.Write("Введите число N :");
+int numM = int.Parse(Console.ReadLine()!);
+int numN = int.Parse(Console.ReadLine()!);
+NumsFromTo(numM, numN);
