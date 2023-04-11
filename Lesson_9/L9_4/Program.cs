b54f83d8ab11107b5a23ee45e3015e0f9ac46f3d
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-04-12 00:33:29 +0300

    L9_4

---
+++ Lesson_9/L9_4/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Напишите программу, которая на вход принимает два числа A и B, и
+//возводит число А в целую степень B с помощью рекурсии.
+int Power(int A, int B)
+{
+    
+    if(B == 0) return 1;
+    return A* Power(A, B - 1) ;
+}
+Console.Write("Введите число A :");
+int numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B :");
+int numB = int.Parse(Console.ReadLine()!);
+Console.Write(Power(numA,numB));
