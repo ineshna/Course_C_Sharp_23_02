e0ad5418a6440d75eeeeeaa21cfd8d1648c17a00
Inesh Zhumatayeva <inesh.zhumatayeva@gmail.com>
2023-04-12 00:21:01 +0300

    L9_3

---
+++ Lesson_9/L9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая будет принимать на вход число и
+//возвращать сумму его цифр. Использовать рекурсию.
+
+int SumDigits(int N)
+{
+    
+    if(N == 0) return 0;
+    return SumDigits(N / 10) + N % 10;
+}
+Console.Write("Введите число  :");
+int numM = int.Parse(Console.ReadLine()!);
+Console.Write(SumDigits(numM));
